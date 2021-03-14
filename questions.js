8e64fb8fe949ca136ae79f44569db949dde075c3
mamtayadav24 <69733550+mamtayadav24@users.noreply.github.com>
2021-03-14 21:39:33 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,41 @@
+let MCQS = [{
+    question: "Who was the first president of the USA?",
+    choice1: "George Washington",
+    choice2: "John Adams",
+    choice3: "Thomas jefferson",
+    choice4: "James Madison",
+    answer: 1
+}, 
+            {
+            question: "Which crop is sown on the largest area in India?",
+    choice1: "Rice",
+    choice2: "Wheat",
+    choice3: "Sugarcane",
+    choice4: "Maize",
+    answer: 1
+},
+            {
+            question: "Who is the famous Ramcharitramanas story of Lord Ram?",
+    choice1: "valmiki",
+    choice2: "Tulsidas",
+    choice3: "Surdash",
+    choice4: "None of the above",
+    answer: 2
+},
+            {
+            question: "Which is the correct CSS syntax?",
+    choice1: "body{color:black}",
+    choice2: "{body{color:black}",
+    choice3: "body={color:black}",
+    choice4: "body:color{black}",
+    answer: 0
+},
+            {
+            question: "How do you insert a comment in a CSS file?",
+    choice1: "/*This is Comment*/",
+    choice2: "//This Is Comment",
+    choice3: "<!--- This Is Comment --->",
+    choice4: "//This Is Comment//",
+    answer: 1
+}
+];
